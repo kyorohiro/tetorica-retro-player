779a83eeb259a2eeebd69f4a7f9a54292989dd6e
kyorohiro <kyorohiro@gmail.com>
2026-06-01 05:17:37 +0900

    v0.5.1

--- docs/demo/assets/BufferResource-DT8BmZBb.js
+++ docs/demo/assets/BufferResource-DT8BmZBb.js
@@ -1,4 +1,4 @@
-import{a6 as U,O as g,a3 as S,E as I,a2 as h}from"./index-2Ad7GX7G.js";import{S as c}from"./RenderTargetSystem-DTUkUpgq.js";import{B as _,a as m}from"./Geometry-BQSPqHY7.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{a6 as U,O as g,a3 as S,E as I,a2 as h}from"./index-B4CIpZBh.js";import{S as c}from"./RenderTargetSystem-DNXqqjvb.js";import{B as _,a as m}from"./Geometry-ZuhdWwdD.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "0.4.10";
+const APP_VERSION = "0.5.1";
 const CACHE_PREFIX = "tetorica-retro-player-";
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 
