726a22e63ab4b275849855fce26ab5a5c9bf6f40
kyorohiro <kyorohiro@gmail.com>
2026-05-31 11:53:55 +0900

    v0.2.7

--- docs/demo/assets/BufferResource-Bh0-PALJ.js
+++ docs/demo/assets/BufferResource-Bh0-PALJ.js
@@ -1,4 +1,4 @@
-import{a6 as U,O as g,a3 as S,E as I,a2 as h}from"./index-jepO3rtG.js";import{S as c}from"./RenderTargetSystem-Bp_K70js.js";import{B as _,a as m}from"./Geometry-BFExt8YO.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{a6 as U,O as g,a3 as S,E as I,a2 as h}from"./index-DBcyznfx.js";import{S as c}from"./RenderTargetSystem-DBztqicf.js";import{B as _,a as m}from"./Geometry-DE0xPJ4y.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
