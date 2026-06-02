c4460cef59682a37c8fc4ed429858ad941f3bdfb
kyorohiro <kyorohiro@gmail.com>
2026-06-03 07:36:05 +0900

    add android dev env

---
+++ src-tauri/gen/android/app/src/main/java/net/tetorica/retro_player/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.tetorica.retro_player
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
