3480db02070467e28e257fab22daf5c14e37ee8f
Kiyohiro Kawamura <kyorohiro@gmail.com>
2026-06-02 12:46:11 +0900

    extract useXxxxx (#1)
    * save temporary
    
    * try_extract_use_pixi_storage
    
    * asis ok
    
    * asis o
    
    * refactoring is asis ok

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { DialogProvider } from "./useDialog";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <DialogProvider>
-      <App />
-    </DialogProvider>
-  </React.StrictMode>,
+  <DialogProvider>
+    <App />
+  </DialogProvider>,
 );
