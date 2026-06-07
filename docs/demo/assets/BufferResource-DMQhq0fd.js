ad6365f1b1954aa344f43b6614521b4d68130725
kyorohiro <kyorohiro@gmail.com>
2026-06-07 10:11:04 +0900

    update doc

--- docs/demo/assets/BufferResource-DMQhq0fd.js
+++ docs/demo/assets/BufferResource-DMQhq0fd.js
@@ -1,4 +1,4 @@
-import{a6 as U,O as g,a3 as S,E as I,a2 as h}from"./index-B9vesum0.js";import{S as c}from"./RenderTargetSystem-DJgQUb44.js";import{B as _,a as m}from"./Geometry-Cobh-Wn7.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{a6 as U,O as g,a3 as S,E as I,a2 as h}from"./index-BtbpiFd7.js";import{S as c}from"./RenderTargetSystem-C6kTBBEc.js";import{B as _,a as m}from"./Geometry-9uXU4EOa.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
