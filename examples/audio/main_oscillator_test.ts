5c8892459d5ed104450d185e49c4948b124e0d77
kyorohiro <kyorohiro@gmail.com>
2026-06-15 16:21:17 +0900

    sample: oscillator --> gain --> output

--- examples/audio/main_oscillator_test.ts
+++ examples/audio/main_oscillator_test.ts
@@ -1,5 +1,33 @@
+//
+// node --experimental-strip-types ./audio/main_oscillator_test.ts
+//
+import { OscillatorNode, AudioContext } from "node-web-audio-api";
+
+//
+// oscillator --> gain --> output
 const main = () => {
     console.log("Hello, World!");
+    const context = new AudioContext();
+    // oscillator
+    const oscillator = new OscillatorNode(context);
+    oscillator.type = "sawtooth";
+    oscillator.frequency.setValueAtTime(220, context.currentTime);
+
+    // gaiin
+    const gain = context.createGain();
+    gain.gain.setValueAtTime(0.03, context.currentTime);
+
+    // connect nodes (oscillator --> gain --> output)
+    oscillator.connect(gain);
+    gain.connect(context.destination);
+
+    // start oscillator and stop after 3 seconds
+    oscillator.start();
+    setTimeout(() => {
+        oscillator.stop();
+        console.log("Oscillator stopped.");
+    }, 3000);
+    
 }
 
 main();
