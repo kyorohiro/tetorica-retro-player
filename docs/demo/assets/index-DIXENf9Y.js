79eb2e7c7b9d3fcacb5c4cfe3b016694a612422b
kyorohiro <kyorohiro@gmail.com>
2026-07-03 12:13:03 +0900

    v0.34.3

---
+++ docs/demo/assets/index-DIXENf9Y.js
@@ -0,0 +1,1 @@
+import{l as n}from"./index-DcKN6ADu.js";async function r(e={}){return typeof e=="object"&&Object.freeze(e),await n("plugin:dialog|open",{options:e})}export{r as open};
--- docs/demo/sw.js
+++ docs/demo/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "0.34.2-mr3nemld";
+const APP_VERSION = "0.34.3-mr4cwzsj";
 const CACHE_PREFIX = "tetorica-retro-player-";
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 
