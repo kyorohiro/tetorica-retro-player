5d5cc4b5578be0ab76f3fa5087460dc13c6f50ba
kyorohiro <kyorohiro@gmail.com>
2026-08-13 20:47:59 +0900

    v0.42.32

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const APP_VERSION = "0.42.32";
-const APP_BUILD_ID = "0.42.32-20260813113309";
+const APP_BUILD_ID = "0.42.32-20260813114232";
 const CACHE_PREFIX = "tetorica-retro-player-";
 const CACHE_NAME = `${CACHE_PREFIX}${APP_BUILD_ID}`;
 
