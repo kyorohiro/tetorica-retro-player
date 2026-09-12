0eb3d185c9172abb4edda78454919a22197dd3dc
kyorohiro <kyorohiro@gmail.com>
2026-09-13 02:20:32 +0900

    v0.44.4

--- docs/demo/assets/index-tlB9H5Bm.js
+++
@@ -1,1 +0,0 @@
-import{G as n}from"./index-BryjGyas.js";async function r(e={}){return typeof e=="object"&&Object.freeze(e),await n("plugin:dialog|open",{options:e})}export{r as open};
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "0.44.3";
-const APP_BUILD_ID = "0.44.3-20260912163518";
+const APP_VERSION = "0.44.4";
+const APP_BUILD_ID = "0.44.4-20260912171934";
 const CACHE_PREFIX = "tetorica-retro-player-";
 const CACHE_NAME = `${CACHE_PREFIX}${APP_BUILD_ID}`;
 
